2eaa5c15197d5824a9b5a085418cb1474df7f284
plasmaa0 <plasmaa0@gmail.com>
2023-06-08 17:41:30 +0300

    asd

--- .start.sh
+++ .start.sh
@@ -1,4 +1,5 @@
 cd ~/.exbash; 
 python3 exbash.py;
-find ~ | grep -v ".exbash" | grep -v ".start.sh"| grep -v ".bash_logout"| grep -v ".bashrc"| grep -v ".profile"| grep -v ".cache"| grep -v ".bash_history"| grep -v ".lesshst" | grep "./" | xargs rm -rf
+cd ~;
+find . | grep -v ".exbash" | grep -v ".start.sh"| grep -v ".bash_logout"| grep -v ".bashrc"| grep -v ".profile"| grep -v ".cache"| grep -v ".bash_history"| grep -v ".lesshst" | grep "./" | xargs rm -rf
 exit
